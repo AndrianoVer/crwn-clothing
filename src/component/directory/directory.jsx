33f8dac545e21f628cb4fdbc16bf289e3ec01592
Andrew Verbitsky <averbitskiy5@gmail.com>
2021-07-07 20:04:03 +0300

    broken homepage to smaller components

--- src/component/directory/directory.jsx
+++ src/component/directory/directory.jsx
@@ -45,11 +45,11 @@ class Directory extends React.Component {
             <div className="directory-menu">
                 {
                     this.state.sections.map(({title, imageUrl, id, size }) => (
-                        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}  />
+                        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
                     ))
                 }
             </div>
-        )
+        );
     }
 };
 
