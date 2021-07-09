e05715139d1e9c3781a42a7062fb1fc5f8013cd0
Andrew Verbitsky <averbitskiy5@gmail.com>
2021-07-09 20:31:33 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
+import { Switch, Route } from 'react-router-dom'
 import HomePage from './pages/homepage';
+
+
+const HatsPage = () => {
+  return (
+    <div>
+      <h1>HATS PAGE</h1>
+    </div>
+  )  
+};
+
+// export default HatsPage;
 
 function App() {
   return (
     <div className="App">
-      <HomePage/>
+      <Switch>
+        <Route path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>      
     </div>
   );
 }
