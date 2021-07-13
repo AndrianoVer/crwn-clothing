27cfbe147bfb3d250ae0c4c97d93fa33ade90f3a
Andrew Verbitsky <averbitskiy5@gmail.com>
2021-07-13 22:53:10 +0300

    add collection-preview page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import { Switch, Route } from 'react-router-dom'
 import HomePage from './pages/homepage';
-import ShopPage from './pages/shop';
+import ShopPage from './pages/shop'
 
 
 function App() {
   return (
     <div className="App">
       <Switch>
-        <Route path="/" component={HomePage} />
+        <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
       </Switch>      
     </div>
---
+++ src/component/collection-preview/collection-preview.jsx
@@ -0,0 +1,21 @@
+import './collection-preview.scss'
+
+const CollectionPreview = ({ title, items }) => {
+    return(
+        <div className="collection-preview">
+            <h1>{title.toUpperCase()}</h1>
+            <div className="preview">
+                {items
+                    .filter((item, idx) => idx < 4)
+                    .map(item => {
+                        return (
+                            <div key={item.id}>{item.name}</div>
+                        )                        
+                    })
+                }
+            </div>
+        </div>
+    )
+}
+
+export default CollectionPreview;
