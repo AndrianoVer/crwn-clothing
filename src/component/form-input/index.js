7cae160d5f8ab51aef4cac446bc4056d33422304
Andrew Verbitsky <averbitskiy5@gmail.com>
2021-07-23 00:33:58 +0300

    adding styles for sign in form

---
+++ src/component/form-input/index.js
@@ -0,0 +1,2 @@
+import FormInput from './form-input.jsx'
+export default FormInput;
--- src/component/sign-in/sign-in.jsx
+++ src/component/sign-in/sign-in.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './sign-in.scss';
+import FormInput from '../form-input'
 
 class SignIn extends React.Component {
     constructor(props) {
@@ -28,21 +29,21 @@ class SignIn extends React.Component {
                 <span>Sign in with your email and password</span>
 
                 <form onSubmit={this.handleSubmit}>
-                    <input name='email'
+                    <FormInput
+                           name='email'
                            type='email'
                            value={this.state.email}
-                           onChange={this.handleChange}
+                           handleChange={this.handleChange}
+                           label="email"
                            required />
 
-                    <label>Email</label>
-
-                    <input name='password'
+                    <FormInput 
+                           name='password'
                            type='password'
                            value={this.state.password}
-                           onChange={this.handleChange}
+                           handleChange={this.handleChange}
+                           label="password"
                            required />
-
-                    <label>Password</label>
 
                     <input type='submit'
                            value='Submit Form' />
