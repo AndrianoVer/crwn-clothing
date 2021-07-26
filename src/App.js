a820b2c552a2f997a83b14d4d634b6f1dba43373
Andrew Verbitsky <averbitskiy5@gmail.com>
2021-07-27 00:33:28 +0300

    adding auth to our project

--- src/App.js
+++ src/App.js
@@ -4,19 +4,44 @@ import HomePage from './pages/homepage';
 import ShopPage from './pages/shop';
 import Header from './component/header/';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up';
+import  { auth } from './firebase/firebase.utils';
+import React from 'react';
 
 
-function App() {
-  return (
-    <div className='App'>
-      <Header />
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={ShopPage} />
-        <Route path='/signin' component={SignInAndSignUpPage} />
-      </Switch>      
-    </div>
-  );
-}
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state =  {
+      currentUser: null
+    }
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setStacte({ currentUser: user });
+      console.log(user);
+    })
+  }
+
+  componentWillMount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route path='/signin' component={SignInAndSignUpPage} />
+        </Switch>      
+      </div>
+    );
+  }
+}  
 
 export default App;
