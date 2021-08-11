eb125dc3451d7eeb447d39d44b84e253e4823b36
Andrew Verbitsky <averbitskiy5@gmail.com>
2021-08-11 22:21:34 +0300

    mapDisptchToProps

--- src/App.js
+++ src/App.js
@@ -1,40 +1,35 @@
 import React from 'react';
-import './App.css';
-import { Switch, Route } from 'react-router-dom'
+import { connect } from 'react-redux';
+import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage';
 import ShopPage from './pages/shop';
 import Header from './component/header/';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up';
-import  { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import { setCurrentUser } from './redux/user/user.actions'
+
+import './App.css';
 
 
 
 class App extends React.Component {
-  constructor() {
-    super();
-
-    this.state =  {
-      currentUser: null
-    };
-  }
 
   unsubscribeFromAuth = null;
 
   componentDidMount() {
+    const {setCurrentUser} = this.props;
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot(snapShot => {
-          this.setState({
-            currentUser: {
+          this.props.setCurrentUser({
               id: snapShot.id,
               ...snapShot.data()
-            }
-          });
+            })
         });
       } else {
-        this.setState({ currentUser: userAuth });
+        setCurrentUser(userAuth);
       }
     })
   }
@@ -57,4 +52,8 @@ class App extends React.Component {
   }
 }  
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+})
+
+export default connect(null, mapDispatchToProps)(App);
