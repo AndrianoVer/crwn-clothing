451711e676fbf9554e797e136f85e259f3fb8f00
Andrew Verbitsky <averbitskiy5@gmail.com>
2021-08-19 23:47:22 +0300

    adding some style to collection-item

--- src/component/collection-item/collection-item.js
+++ src/component/collection-item/collection-item.js
@@ -1,16 +1,20 @@
 import './collection-item.scss';
+import CustomButton from '../custom-button';
 
 const CollectionItem = ({ id, name, price, imageUrl }) => {
     return (
         <div className='collection-item'>
             <div 
                 className='image'
-                style={{backgroundImage: `url(${imageUrl})`}}>
+                style={{
+                    backgroundImage: `url(${imageUrl})`
+                }}>
             </div>
         <div className='collection-footer'>
             <span className='name'>{ name }</span>
             <span className='price'>{ price }</span>
         </div>
+        <CustomButton inverted> Add to Cart </CustomButton>
     </div>
     )
     
--- src/component/custom-button/custom-button.jsx
+++ src/component/custom-button/custom-button.jsx
@@ -1,8 +1,10 @@
 import './custom-button.scss';
 
-const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => {
+const CustomButton = ({ children, isGoogleSignIn, inverted, ...otherProps }) => {
     return (
-        <button className={`${isGoogleSignIn ? 'google-sign-in' : '' } custom-button`}
+        <button 
+            className={`${inverted ? 'inverted' : ''}
+             ${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`}
         {...otherProps}>
             {children}
         </button>
