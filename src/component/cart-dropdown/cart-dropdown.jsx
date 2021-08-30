35d2961f987e4237112f548c18ebe0c2626786b8
Andrew Verbitsky <averbitskiy5@gmail.com>
2021-08-30 22:28:11 +0300

    solve problem with checkout page

--- src/component/cart-dropdown/cart-dropdown.jsx
+++ src/component/cart-dropdown/cart-dropdown.jsx
@@ -4,12 +4,13 @@ import { createStructuredSelector } from 'reselect'
 import CustomButton from '../custom-button';
 import CartItem from '../cart-item';
 import { selectCartItems } from '../../redux/cart/cart.selectors';
+import { toggleCartHidden } from '../../redux/cart/cart.actions';
 import { withRouter } from 'react-router-dom';
 
 
 import './cart-dropdown.scss';
 
-const CartDropdown = ({cartItems, history  }) => (
+const CartDropdown = ({cartItems, history, dispatch  }) => (
     <div className='cart-dropdown'>
         <div className='cart-items'>
             {cartItems.length ?
@@ -20,7 +21,12 @@ const CartDropdown = ({cartItems, history  }) => (
                 <span className='empty-message'>Your cart is empty</span>
             )}
         </div>
-        <CustomButton onClick={() => history.push('/checkout')}>GO TO CHECKOUT</CustomButton>
+        <CustomButton onClick={() => {
+            history.push('/checkout');
+            dispatch(toggleCartHidden());
+        }}>
+            GO TO CHECKOUT
+        </CustomButton>
     </div>
 );
 
